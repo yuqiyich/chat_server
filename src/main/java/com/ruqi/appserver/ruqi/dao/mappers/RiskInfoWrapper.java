7cb84219de987b0d829e44625863790002627845
yich <yuqi@ruqimobility.com>
2020-03-10 19:17:13 +0800

    feat:增加上报安全接口字段

--- src/main/java/com/ruqi/appserver/ruqi/dao/mappers/RiskInfoWrapper.java
+++ src/main/java/com/ruqi/appserver/ruqi/dao/mappers/RiskInfoWrapper.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface RiskInfoWrapper {
 
-    @Insert("insert into risk_record(risk_type,user_id,device_id,risk_detail,create_time,device_brand,system_version,app_version,net_state,location_lat,location_lng,channel,app_id) " +
-            "values(#{riskType},#{userId},#{deviceId},#{riskDetail},#{createTime},#{deviceBrand},#{systemVersion},#{appVersion},#{netState},#{locationLat},#{locationLng},#{channel},#{appId})")
+    @Insert("insert into risk_record(risk_type,user_id,device_id,risk_detail,create_time,device_brand,system_version,app_versionname,net_state,location_lat,location_lng,channel,app_id,app_versioncode,device_model,scene) " +
+            "values(#{riskType},#{userId},#{deviceId},#{riskDetail},#{createTime},#{deviceBrand},#{systemVersion},#{appVersionName},#{netState},#{locationLat},#{locationLng},#{channel},#{appId},#{appVersionCode},#{deviceModel},#{scene})")
     int insert(RiskInfo riskInfo);
 }
--- src/main/java/com/ruqi/appserver/ruqi/utils/GenerateAppKeyUtil.java
+++ src/main/java/com/ruqi/appserver/ruqi/utils/GenerateAppKeyUtil.java
@@ -5,6 +5,7 @@ import sun.security.provider.MD5;
 public class GenerateAppKeyUtil {
     public static final String  salt="2020*.ruqi#!888";
     public static String appName= "com.ruqi.travel";
+//    public static String appName= "com.tencent.gac.driver";
     public static void main(String[] args) {
         System.out.println("app packagename:"+appName+"=====appkey:"+Md5Util.md5StrBySalt(appName,salt).substring(0,16));
 
