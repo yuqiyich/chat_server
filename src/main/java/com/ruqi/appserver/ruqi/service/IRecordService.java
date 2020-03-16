ff931d10b0a64c0074e0f1bce3771de184dc496d
yich <yuqi@ruqimobility.com>
2020-03-16 19:59:30 +0800

    feat:报警列表查询50%

--- src/main/java/com/ruqi/appserver/ruqi/service/IRecordService.java
+++ src/main/java/com/ruqi/appserver/ruqi/service/IRecordService.java
@@ -6,6 +6,7 @@ import com.ruqi.appserver.ruqi.bean.RiskInfo;
 import com.ruqi.appserver.ruqi.bean.UserEntity;
 
 import java.util.Date;
+import java.util.List;
 
 /**
  * 记录埋点的服务
@@ -13,4 +14,6 @@ import java.util.Date;
 public interface IRecordService {
 
     void saveRecord(RecordInfo<RiskInfo> data, Date uploadTime,String requestIp);
+    List<RecordInfo<RiskInfo>> queryList(int pageIndex, int limit, RecordInfo<RiskInfo> params);
+    int queryTotalSize(RecordInfo<RiskInfo> params);
 }
