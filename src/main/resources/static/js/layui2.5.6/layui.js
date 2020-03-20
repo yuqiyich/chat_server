3427884268b61a9b39ba17082687d10827a31aec
zhangyu <420369556@qq.com>
2020-03-20 17:43:33 +0800

    fix：指定api返回中文不乱码

--- src/main/resources/static/js/layui2.5.6/layui.js
+++ src/main/resources/static/js/layui2.5.6/layui.js
@@ -207,29 +207,10 @@
         }), i)
     }, e.layui = new o
 }(window);
-
-
-
-
-
 //-------------------------------张宇自定义-----------------------------------
 function addZero(num) {
     return num < 10 ? '0' + num : num;
 };
-
-function myTime(date) {
-    var arr = date.split("T");
-    var d = arr[0];
-    var darr = d.split('-');
-
-    var t = arr[1];
-    var tarr = t.split('.000');
-    var marr = tarr[0].split(':');
-
-    var dd = parseInt(darr[0]) + "/" + parseInt(darr[1]) + "/" + parseInt(darr[2]) + " " + parseInt(marr[0]) + ":" + parseInt(marr[1]) + ":" + parseInt(marr[2]);
-    return dd;
-}
-
 function formatDateTime(date) {
     if (null == date) {
         return "";
@@ -249,4 +230,16 @@ function formatDateTime(date) {
     var s = this.addZero(time.getSeconds());
 
     return Y + M + D + h + m + s;
+};
+function myTime(date) {
+    var arr = date.split("T");
+    var d = arr[0];
+    var darr = d.split('-');
+
+    var t = arr[1];
+    var tarr = t.split('.000');
+    var marr = tarr[0].split(':');
+
+    var dd = parseInt(darr[0]) + "/" + parseInt(darr[1]) + "/" + parseInt(darr[2]) + " " + parseInt(marr[0]) + ":" + parseInt(marr[1]) + ":" + parseInt(marr[2]);
+    return dd;
 };
