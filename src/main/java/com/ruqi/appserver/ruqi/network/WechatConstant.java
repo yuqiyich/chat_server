fb6b5606aaf084ab200b455884ed224bccf154ed
zhangyu <420369556@qq.com>
2020-05-08 14:42:28 +0800

    feat：替换新的微信服务号的AppSecret

--- src/main/java/com/ruqi/appserver/ruqi/network/WechatConstant.java
+++ src/main/java/com/ruqi/appserver/ruqi/network/WechatConstant.java
@@ -2,7 +2,7 @@ package com.ruqi.appserver.ruqi.network;
 
 public class WechatConstant {
     public static final String APP_ID = "wx88b0931b54f609e3";
-    public static final String SECRET = "745cc18dcf6517451d8297fb51c343dd";
+    public static final String SECRET = "42db0f1011208c75a9066ca7028147e3";
 
     // 微信模板消息，报警通知
     public static final String TEMPLATE_ID = "3Iulg0CDRg-ED-NPUOP4aqeFJUiMl2NZi6eZ4aS9VLo";
