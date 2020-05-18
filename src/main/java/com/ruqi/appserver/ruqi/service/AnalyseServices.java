fa48eb52260b6228f9da598240e41f4a91736a62
yich <yuqi@ruqimobility.com>
2020-05-18 11:59:04 +0800

    fix:去掉每一个小时统计；改为每天10:30查看前一天统计

--- src/main/java/com/ruqi/appserver/ruqi/service/AnalyseServices.java
+++ src/main/java/com/ruqi/appserver/ruqi/service/AnalyseServices.java
@@ -4,6 +4,7 @@ import com.ruqi.appserver.ruqi.bean.AppInfo;
 import com.ruqi.appserver.ruqi.controller.WechatController;
 import com.ruqi.appserver.ruqi.dao.mappers.AppInfoWrapper;
 import com.ruqi.appserver.ruqi.dao.mappers.RiskInfoWrapper;
+import com.ruqi.appserver.ruqi.utils.DateTimeUtils;
 import com.ruqi.appserver.ruqi.utils.IpUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,11 +22,16 @@ import java.util.List;
 
 @Component
 @EnableScheduling   // 1.开启定时任务
-@EnableAsync     //开启异步线程
 public class AnalyseServices {
+    Logger logger = LoggerFactory.getLogger(getClass());
 
     private static final long INTERVAL = 1000 * 60 * 60;//上报时间间隔
-    private static final long MAX_THRESHOLD = 50;//最大上报数
+    private static final long MAX_DRIVER_THRESHOLD_OF_DAY = 50;//司机端最大上报数
+    private static final long MAX_CLIENT_THRESHOLD_OF_DAY = 50;//最大上报数
+    private static final long APP_CLIENT = 2;//乘客端应用id
+    private static final long APP_DRIVER = 1;//司机端应用id
+    private static final String CRON_REG = "0 0 10 * * ? ";//
+
 
     @Autowired
     RiskInfoWrapper riskInfoWrapper;
@@ -39,20 +45,18 @@ public class AnalyseServices {
     @Value("${spring.profiles.active}")
     private String mEnv = "";
 
-    @Async("taskExecutor")
-    @Scheduled(fixedDelay = INTERVAL)
+    @Scheduled(cron = CRON_REG)
     public void periodCheckSecurity() throws InterruptedException {
         if ("prod".equals(mEnv)) {
-            Logger logger = LoggerFactory.getLogger(getClass());
             List<AppInfo> appInfos = appInfoWrapper.getAllApps();
             if (!CollectionUtils.isEmpty(appInfos)) {
                 for (AppInfo appInfo : appInfos) {
-                    if (appInfo.getAppId() == 1 || appInfo.getAppId() == 2) {
-                        int count = riskInfoWrapper.countSecurityNum(appInfo.getAppId(), new Date(System.currentTimeMillis() - INTERVAL), new Date());
-                        if (count >= MAX_THRESHOLD) {
-                            logger.debug("risk count:" + count);
+                    if (appInfo.getAppId() == APP_CLIENT || appInfo.getAppId() == APP_DRIVER) {
+                        int count = riskInfoWrapper.countSecurityNum(appInfo.getAppId(), DateTimeUtils.getYesterdayStartDate(),  DateTimeUtils.getYesterdayEndDate());
+                        if (count >= 0) {//不设阈值
+                            logger.info("risk count:" + count);
                             mWechatController.sendSecurityTemplateMsg(appInfo.getAppName(), "设备风险",
-                                    "在过去的" + (INTERVAL / 60 / 1000) + "分钟内" + count + "条设备风险数据[" + mEnv + "],发送ip:" + IpUtil.getLocalIP(), "请至APP记录平台查看完整详细信息", null);
+                                    "在过去的"  + DateTimeUtils.getYesterday() +"一天内总共有"+ count + "条设备风险数据[" + mEnv + "],发送ip:" + IpUtil.getLocalIP(), "请至APP记录平台查看完整详细信息", null);
                         } else {
                             logger.info("appId[" + appInfo.getAppId() + "]periodCheckSecurity method run,find no risk");
                         }
--- src/main/java/com/ruqi/appserver/ruqi/utils/DateTimeUtils.java
+++ src/main/java/com/ruqi/appserver/ruqi/utils/DateTimeUtils.java
@@ -1,6 +1,7 @@
 package com.ruqi.appserver.ruqi.utils;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class DateTimeUtils {
@@ -9,4 +10,35 @@ public class DateTimeUtils {
         SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         return df.format(t);
     }
+
+
+    public static String getYesterday(){
+        Calendar cal=Calendar.getInstance();
+        cal.add(Calendar.DATE,-1);
+        Date d=cal.getTime();
+        SimpleDateFormat sp=new SimpleDateFormat("yyyy-MM-dd");
+        return  sp.format(d);//获取昨天日期
+    }
+
+    public static Date getYesterdayStartDate(){
+        Calendar cal=Calendar.getInstance();
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        return  cal.getTime();
+    }
+
+    public static Date getYesterdayEndDate(){
+        Calendar cal=Calendar.getInstance();
+        cal.set(Calendar.HOUR_OF_DAY, 23);
+        cal.set(Calendar.MINUTE, 59);
+        cal.set(Calendar.SECOND, 59);
+        return  cal.getTime();
+    }
+
+
+
+
+
+
 }
