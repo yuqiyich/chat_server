caffa21abe0f37b5628787663140568bda82a42a
yich <yuqi@ruqimobility.com>
2020-07-30 14:50:35 +0800

    feat:修改参数zookeeper连接参数

--- src/main/java/com/ruqi/appserver/ruqi/geomesa/HbaseConnectConfig.java
+++ src/main/java/com/ruqi/appserver/ruqi/geomesa/HbaseConnectConfig.java
@@ -14,6 +14,6 @@ public class HbaseConnectConfig {
 
     /****************相关配置的默认值 start************************/
     //zookeeper的的连接集群配置
-    static String VALUE_ZOOKEEPER = "hdpcommon01:2181,hdpcommon02:2181,hdpcommon03:2181";
+    static String VALUE_ZOOKEEPER = "10.10.28.248:2181,10.10.28.247:2181,10.10.28.31:2181";
     /****************相关配置的默认值 end************************/
 }
